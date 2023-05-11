c1e3b6a9d4d1df1028ffd0bbe8e7f3fc9a1d8821
guava <guavareigen2@gmail.com>
2023-05-11 14:50:59 +0530

    Added Email trigger function

--- main.py
+++ main.py
@@ -1,11 +1,13 @@
 import cv2
 import time
+from email_part import send_email
 
 first_frame = None
 video = cv2.VideoCapture(0)
 time.sleep(1)
+status_list=[]
 while True:
-
+    status = 0
     check, frame = video.read()
     gray_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
     gray_frame_blur = cv2.GaussianBlur(gray_frame,(21,21),0)
@@ -15,17 +17,26 @@ while True:
         first_frame = gray_frame_blur
 
     delta_frame = cv2.absdiff(first_frame, gray_frame_blur)
-    thresh_frame = cv2.threshold(delta_frame, 45, 255, cv2.THRESH_BINARY)[1]
+    thresh_frame = cv2.threshold(delta_frame, 60, 255, cv2.THRESH_BINARY)[1]
     dil_frame = cv2.dilate(thresh_frame, None, iterations=2)
     cv2.imshow("My Video", dil_frame)
 
     contours, check = cv2.findContours(dil_frame,cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
 
     for contour in contours:
-        if cv2.contourArea(contour) < 10000:
+        if cv2.contourArea(contour) < 5000:
             continue
-        x,y,w,h = cv2.BoundingRect(contour)
-        cv2.rectangle(frame,(x,y),(x+w, y+h),(0, 255, 0), 3)
+        x, y, w, h = cv2.boundingRect(contour)
+        rectangle = cv2.rectangle(frame, (x, y), (x+w, y+h), (0, 255, 0), 3)
+
+        if rectangle.any():
+            status = 1
+            send_email()
+    status_list.append(status)
+    status_list = status_list[-2:]
+
+    if status_list[0] == 1 and status_list[1] == 1:
+        send_email()
 
     cv2.imshow("Video",frame)
     key = cv2.waitKey(1)
